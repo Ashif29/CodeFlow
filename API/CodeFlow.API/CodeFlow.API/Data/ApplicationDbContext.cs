841a7c2a916a84b8258d8106a87cde97ec4d690b
ashif29 <asifcr75@gmail.com>
2024-02-21 12:43:01 +0600

    Added ApplicationDbContext Class

---
+++ API/CodeFlow.API/CodeFlow.API/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CodeFlow.API.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodeFlow.API.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet <BlogPost> BlogPosts { get; set; }
+        public DbSet <Category> Categories { get; set; }
+    }
+}
