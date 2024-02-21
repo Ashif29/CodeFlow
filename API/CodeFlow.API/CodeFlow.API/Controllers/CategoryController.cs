840c4624bd1f0c9fe79f31509a7037c8201f1e60
ashif29 <asifcr75@gmail.com>
2024-02-21 13:56:31 +0600

    Create POST Categories Action Method (Mapping Between DTO and Domain Model)

--- API/CodeFlow.API/CodeFlow.API/Controllers/CategoryController.cs
+++ API/CodeFlow.API/CodeFlow.API/Controllers/CategoryController.cs
@@ -1,11 +1,46 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using CodeFlow.API.Data;
+using CodeFlow.API.Models.Domain;
+using CodeFlow.API.Models.DTO;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CodeFlow.API.Controllers
 {
+    // https://loclhost:xxxx/api/categories
+
     [Route("api/[controller]")]
     [ApiController]
     public class CategoryController : ControllerBase
     {
+        private readonly ApplicationDbContext dbContext;
+
+        public CategoryController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory(CreateCategoryRequestDto request)
+        {
+            // Map DTO to Domain Model
+            var category = new Category
+            {
+                Name = request.Name,
+                UrlHandle = request.UrlHandle
+            };
+
+            await dbContext.Categories.AddAsync(category);
+            await dbContext.SaveChangesAsync();
+
+            // Map Domain Model to DTO
+            var response = new CategoryDto
+            {
+                Id = category.Id,
+                Name = category.Name,
+                UrlHandle = category.UrlHandle
+            };
+
+            return Ok(response);
+        }
     }
 }
